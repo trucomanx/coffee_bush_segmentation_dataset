cdce8934a52d348a335f5e14177aa851132eaa79
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2026-04-05 02:30:42 -0300

    ordenando

--- example/example-show.py
+++ example/example-show.py
@@ -1,19 +1,19 @@
 import numpy as np
-
-#imagem = np.load('../output/train/images/1_0_320.npy')
-imagem = np.load('../output/train/labels/1_0_320_bushes.npy')
-#imagem = np.load('../output/train/labels/1_16_176_map.npy')
-
-
-import cv2
-cv2.imshow('Imagem RGB', imagem[:,:,0:3])
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
-'''
 import matplotlib.pyplot as plt
-plt.imshow(imagem)
-plt.axis('off')  # Desativa os eixos
+
+# Carregar imagem
+# imagem = np.load('../output/train/images/1_0_320.npy')
+imagem = np.load('../output/train/labels/1_0_320_bushes.npy')
+# imagem = np.load('../output/train/labels/1_16_176_map.npy')
+
+# Se a imagem tiver múltiplos canais (ex: RGB)
+if len(imagem.shape) == 3:
+    imagem_plot = imagem[:, :, 0:3]
+else:
+    imagem_plot = imagem
+
+# Mostrar imagem
+plt.imshow(imagem_plot)
+plt.title("Imagem")
+plt.axis('off')  # remove eixos
 plt.show()
-'''
