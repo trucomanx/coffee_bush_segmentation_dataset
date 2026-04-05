842ea3ce07ae10b945f9f459651f1bc7f046b207
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2026-04-05 03:50:50 -0300

    ordenando

--- datasets/to-preprocessed.sh
+++ datasets/to-preprocessed.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+RAW_TRAIN_VAL="0-raw/train-val"
+PRE_TRAIN_VAL="1-preprocessed/train-val/images_delete"
+
 python3 ../src/prog_tiff2bmp.py \
-  --input-dir "0-raw/train-val" \
-  --output-dir "1-preprocessed/train-val/images_delete" \
+  --input-dir $RAW_TRAIN_VAL \
+  --output-dir $PRE_TRAIN_VAL \
   --names 'Red.bmp' 'Green.bmp' 'Blue.bmp' 'Nir.bmp' 'RedEdge.bmp'
